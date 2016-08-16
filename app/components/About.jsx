e23ff9e868865d5cb74323f1d9ab296983700224
TychoCRD <rtcotton@gmail.com>
2016-08-16 14:23:59 +0800

    Style About and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,19 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page</p>
+      <h1 className="text-center">About</h1>
+      <p className="text-justify">This is a simple single-page web app built on the JavaScript library React as a part of Andrew Mead's <i>The Complete Web App Developer Course</i>. Links for the technologies used and its github repository can be found below. </p>
+      <div className="menu-centered">
+        <ul className="menu">
+          <li><a href="https://github.com/TychoCRD/ReactWeather" target="_blank">Github</a></li>
+          <li><a href="https://www.udemy.com/the-complete-react-web-app-developer-course" target="_blank">Udemy Course</a></li>
+        </ul>
+        <ul className="menu">
+          <li><a href="https://facebook.github.io/react/" target="_blank">React</a></li>
+          <li><a href="http://foundation.zurb.com/" target="_blank">Foundation</a></li>
+          <li><a href="https://openweathermap.org" target="_blank">Open Weather Map</a></li>
+        </ul>
+      </div>
     </div>
   );
 };
